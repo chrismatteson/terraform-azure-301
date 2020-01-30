7bb7bc94b9289c8645cee42d9c4b92493c77fed5
Chris Matteson <chris.matteson@gmail.com>
2020-01-29 22:17:37 -0800

    updates

--- student/helmvault.tf
+++ student/helmvault.tf
@@ -34,37 +34,37 @@ resource "helm_release" "vault" {
 resource "null_resource" "vault" {
   provisioner "local-exec" {
     command = <<EOT
-echo "$(terraform011 output kube_config)" > ./azurek8s;
+echo "$(../terraform output kube_config)" > ./azurek8s;
 export KUBECONFIG=./azurek8s;
 export VAULT_POD=$(kubectl get pods --namespace default -l "app=vault" -o jsonpath="{.items[0].metadata.name}");
 export VAULT_TOKEN=$(kubectl logs $VAULT_POD | grep 'Root Token' | cut -d' ' -f3);
 export VAULT_ADDR=http://127.0.0.1:8200;
 kubectl port-forward $VAULT_POD 8200 &
 sleep 10s;
-echo $VAULT_TOKEN | vault login -;
-vault secrets enable database;
-vault write database/config/postgres \
+echo $VAULT_TOKEN | ../vault login -;
+../vault secrets enable database;
+../vault write database/config/postgres \
     plugin_name=postgresql-database-plugin \
     allowed_roles="postgres-role" \
     connection_url="postgresql://root:root@${data.kubernetes_service.postgres.spec.0.cluster_ip}:5432/rails_development?sslmode=disable";
-vault write database/roles/postgres-role \
+../vault write database/roles/postgres-role \
     db_name=postgres \
     creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; \
         GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";" \
     default_ttl="1h" \
     max_ttl="24h";
-vault policy write postgres-policy postgres-policy.hcl;
+../vault policy write postgres-policy postgres-policy.hcl;
 kubectl apply -f postgres-serviceaccount.yml;
 export VAULT_SA_NAME=$(kubectl get sa postgres-vault -o jsonpath="{.secrets[*]['name']}");
 export SA_JWT_TOKEN=$(kubectl get secret $VAULT_SA_NAME -o jsonpath="{.data.token}" | base64 --decode; echo);
 export SA_CA_CRT=$(kubectl get secret $VAULT_SA_NAME -o jsonpath="{.data['ca\.crt']}" | base64 --decode; echo);
 export K8S_HOST=${azurerm_kubernetes_cluster.k8s.fqdn};
-vault auth enable kubernetes;
-vault write auth/kubernetes/config \
+../vault auth enable kubernetes;
+../vault write auth/kubernetes/config \
   token_reviewer_jwt="$SA_JWT_TOKEN" \
   kubernetes_host="https://$K8S_HOST:443" \
   kubernetes_ca_cert="$SA_CA_CRT";
-vault write auth/kubernetes/role/postgres \
+../vault write auth/kubernetes/role/postgres \
     bound_service_account_names=postgres-vault \
     bound_service_account_namespaces=default \
     policies=postgres-policy \
