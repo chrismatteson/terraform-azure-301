888b880e23a66359b9e092b7446d98d5966cd1b4
chrismatteson <cmatteson@hashicorp.com>
2020-04-15 23:01:40 -0700

    Update backend.tf

--- student/backend.tf
+++ student/backend.tf
@@ -3,9 +3,6 @@ terraform {
     storage_account_name = "<replace>"
     container_name       = "tfstate"
     key                  = "<replace>.terraform.tfstate"
-    subscription_id      = "<replace>"
-    tenant_id            = "<replace>"
-    client_id            = "<replace>"
-    client_secret        = "<replace>"
+    access_key.          = "<replace>"
   }
 }
